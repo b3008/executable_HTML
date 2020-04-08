0b087b68d69ae21acf75d2ebf529192183dff013
b3008 <amoivikos@gmail.com>
2020-04-09 02:07:42 +0300

    feat: hide screen if autohide was set

--- src/customElements/aa-screen/aa-screen.js
+++ src/customElements/aa-screen/aa-screen.js
@@ -164,7 +164,7 @@ export default class AAScreen extends BaseElement {
         if (typeof e.detail.callback != 'undefined') {
             e.detail.callback(e);
         }
-        if (this.dontHide === false) { this.hide(); }
+        if (this.autohide) { this.hide(); }
 
     }
 
