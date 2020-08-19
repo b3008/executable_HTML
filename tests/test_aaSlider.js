d99f32b2099bb2c8548a92c2197404dd944ac224
b3008 <amoivikos@gmail.com>
2020-08-19 16:54:36 +0300

    fix: uncomment tests for aaSlider

--- tests/test_aaSlider.js
+++ tests/test_aaSlider.js
@@ -28,51 +28,51 @@ describe('aa-slider', () => {
             done();
         });
 
-        // it('sets value', (done) => {
-        //     container.innerHTML = '<aa-slider value="30"></aa-slider>';
-        //     let slider = document.querySelector('aa-slider');
-        //     slider.value = '30';
-        //     assert(slider.value == '30', `value of element is passed into its inputItem`);
-        //     assert(slider.inputItem.value == '30', `value of element is passed into its inputItem`);
+        it('sets value', (done) => {
+            container.innerHTML = '<aa-slider value="30"></aa-slider>';
+            let slider = document.querySelector('aa-slider');
+            slider.value = '30';
+            assert(slider.value == '30', `value of element is passed into its inputItem`);
+            assert(slider.inputItem.value == '30', `value of element is passed into its inputItem`);
 
-        //     slider.inputItem.value = '30';
-        //     assert(slider.value == '30', `setting value of inputItem sets value of element`);
-        //     done();
-        // });
+            slider.inputItem.value = '30';
+            assert(slider.value == '30', `setting value of inputItem sets value of element`);
+            done();
+        });
 
-        // it('gets maxLabel', (done) => {
-        //     container.innerHTML = '<aa-slider max-label="label1"></aa-slider>';
-        //     let slider = document.querySelector('aa-slider');
-        //     assert(slider.maxLabel === 'label1', `label of element is set`);
-        //     // assert(slider.inputItem.maxLabel === 'label1', `label of element is passed into its inputItem`);
-        //     done();
-        // });
+        it('gets maxLabel', (done) => {
+            container.innerHTML = '<aa-slider max-label="label1"></aa-slider>';
+            let slider = document.querySelector('aa-slider');
+            assert(slider.maxLabel === 'label1', `label of element is set`);
+            // assert(slider.inputItem.maxLabel === 'label1', `label of element is passed into its inputItem`);
+            done();
+        });
 
-        // it('sets maxLabel', (done) => {
-        //     container.innerHTML = '<aa-slider value="30"></aa-slider>';
-        //     let slider = document.querySelector('aa-slider');
-        //     slider.maxLabel = 'label1';
-        //     assert(slider.maxLabel === 'label1', `label of element is set`);
-        //     // assert(slider.inputItem.maxlabel === 'label1', `label of element is passed into its inputItem`);
-        //     done();
-        // });
+        it('sets maxLabel', (done) => {
+            container.innerHTML = '<aa-slider value="30"></aa-slider>';
+            let slider = document.querySelector('aa-slider');
+            slider.maxLabel = 'label1';
+            assert(slider.maxLabel === 'label1', `label of element is set`);
+            // assert(slider.inputItem.maxlabel === 'label1', `label of element is passed into its inputItem`);
+            done();
+        });
 
-        // it('gets minLabel', (done) => {
-        //     container.innerHTML = '<aa-slider min-label="label1"></aa-slider>';
-        //     let slider = document.querySelector('aa-slider');
-        //     assert(slider.minLabel === 'label1', `label of element is set`);
-        //     // assert(slider.inputItem.maxLabel === 'label1', `label of element is passed into its inputItem`);
-        //     done();
-        // });
+        it('gets minLabel', (done) => {
+            container.innerHTML = '<aa-slider min-label="label1"></aa-slider>';
+            let slider = document.querySelector('aa-slider');
+            assert(slider.minLabel === 'label1', `label of element is set`);
+            // assert(slider.inputItem.maxLabel === 'label1', `label of element is passed into its inputItem`);
+            done();
+        });
 
-        // it('sets minLabel', (done) => {
-        //     container.innerHTML = '<aa-slider value="30"></aa-slider>';
-        //     let slider = document.querySelector('aa-slider');
-        //     slider.minLabel = 'label1';
-        //     assert(slider.minLabel === 'label1', `label of element is set`);
-        //     // assert(slider.inputItem.label === 'label1', `label of element is passed into its inputItem`);
-        //     done();
-        // });
+        it('sets minLabel', (done) => {
+            container.innerHTML = '<aa-slider value="30"></aa-slider>';
+            let slider = document.querySelector('aa-slider');
+            slider.minLabel = 'label1';
+            assert(slider.minLabel === 'label1', `label of element is set`);
+            // assert(slider.inputItem.label === 'label1', `label of element is passed into its inputItem`);
+            done();
+        });
         
     })
 })
